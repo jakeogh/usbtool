d947c44db1819742388b58965950ed87405bd851
jakeogh <github.com@v6y.net>
2025-01-30 14:20:27 -0700

    auto-commit

--- usbtool/usbtool.py
+++ usbtool/usbtool.py
@@ -81,7 +81,6 @@ def get_usb_id_for_device(device) -> str:
         _l = _l.strip()
         if _l.startswith("ATTRS{idProduct}=="):
             id_product = _l.split('"')[1]
-            # ic(id_product)
             id_vendor = _lines[index + 1].split('"')[1]
             # ic(id_vendor)
             usb_id = f"{id_vendor}:{id_product}"
