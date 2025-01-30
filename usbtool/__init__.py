4c58769327cb17c776d40b4c3ac72dd8c3a4070f
jakeogh <github.com@v6y.net>
2025-01-29 18:36:20 -0700

    auto-commit

--- usbtool/__init__.py
+++ usbtool/__init__.py
@@ -6,4 +6,6 @@ isort:skip_file
 from .usbtool import get_usb_id_dict as get_usb_id_dict
 from .usbtool import get_usb_tty_device_list as get_usb_tty_device_list
 from .usbtool import get_usb_id_for_device as get_usb_id_for_device
-from .usbtool import get_device_for_usb_id as get_device_for_usb_id
+from .usbtool import get_devices_for_usb_id as get_devices_for_usb_id
+
+# from .usbtool import find_device as find_device
