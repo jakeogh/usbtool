d826e615e5e8a2c3efec0f125ed08ad1c2d4f5af
jakeogh <github.com@v6y.net>
2025-02-28 14:22:36 -0700

    auto-commit

--- .autogenerate_readme.sh
+++ .autogenerate_readme.sh
@@ -4,4 +4,4 @@ usbtool find-device --help
 
 usbtool find-device --baud-rate 921600 --command-hex 100253411003 --response-hex 065341
 
-usbtool find-device --serial-number DA1ZDECW
+usbtool find-device --serial-number FT7E7CA6
